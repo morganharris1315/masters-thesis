1fc7c1dfebf46d9566a8369ce4c924245d0d3529
Morgan Harris <morganharris1315@gmail.com>
2026-02-13 14:49:48 +1300

    Rotate region strip labels vertically in exceedance panel plot

--- 03b-blockmaxima-function-modeldata-singlethreshold.R
+++ 03b-blockmaxima-function-modeldata-singlethreshold.R
@@ -355,8 +355,8 @@ plot_rx1day_vs_exceedance_panel <- function(df_panel) {
     theme_thesis +
     theme(
       strip.placement = "outside",
-      strip.text.y = element_text(angle = 0, hjust = 0.5),
-      strip.text.y.left = element_text(angle = 0, hjust = 0),
+      strip.text.y = element_text(angle = 90, hjust = 0.5, vjust = 0.5),
+      strip.text.y.left = element_text(angle = 90, hjust = 0.5, vjust = 0.5),
       panel.spacing = unit(1.1, "lines"),
       panel.border = element_rect(colour = "grey45", fill = NA, linewidth = 0.35),
       axis.line = element_blank()
