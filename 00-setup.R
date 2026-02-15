eb92fe5fe7cc8dbd6d3216db993d3073c9ae0199
Morgan Harris <morganharris1315@gmail.com>
2026-02-16 12:02:30 +1300

    Update 00-setup.R

--- 00-setup.R
+++ 00-setup.R
@@ -45,8 +45,8 @@ fig_height_med   <- 4.8
 fig_height_tall  <- 6.2
 fig_height_full <- 7
 
-fig_width_standard  <- 17.02 / 2.54
-fig_height_standard <- 9.65 / 2.54
+fig_width_standard  <- 16.89 / 2.54
+fig_height_standard <- 19 / 2.54
 
 fig_width_hoz_full  <- 11
 fig_height_hoz_full <- 7.2
