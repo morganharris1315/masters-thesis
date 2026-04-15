dff44e9ba56c4afdf37ddec32bb5cfa5ecc7c2e1
Morgan Harris <morganharris1315@gmail.com>
2026-04-16 08:01:11 +1200

    Update 07-combined-figures.R

--- 07-combined-figures.R
+++ 07-combined-figures.R
@@ -51,7 +51,7 @@ lidar_dir <- file.path(base_raw_dir, "obs_data", "coromandel", "LiDAR data")
 
 figure1_file <- file.path(base_raw_dir, "obs_data", "Figure1_coromandel_context.png")
 figure2_file <- file.path(weatherathome_dir, "Figure2_coromandel_cell_2x3.png")
-figure5_file <- file.path(weatherathome_dir, "Figure5.png")
+figure5_file <- file.path(weatherathome_dir, "Figure5_ge5_heatmap.png")
 
 # Inputs -------------------------------------------------------------------
 matched_cell <- data.frame(lon_index = 30L, lat_index = 16L)
@@ -606,7 +606,6 @@ fp_df <- fp_df %>%
     heavy_days = count_heavy_days(future_files, matched_cell$lon_index, matched_cell$lat_index, heavy_fp)
   )
 
-# Figure 2 panels (c)-(f)
 fp_df_cd_thresholds <- fp_df %>%
   mutate(
     heavy_threshold = heavy_cd,
@@ -630,15 +629,15 @@ p2d <- plot_heavy_hist(hist_fp, panel_tag = "(d)", max_exceed = heavy_days_max)
 p2e <- plot_quadrant_heatmap(cd_df, panel_tag = "(e)", heavy_cutoff = 4L, x_max = heavy_days_max)
 p2f <- plot_quadrant_heatmap(fp_df_cd_thresholds, panel_tag = "(f)", heavy_cutoff = 4L, x_max = heavy_days_max)
 
-# Figure 2e/f alternate framing: at least 5 Heavy days
-p2e_heavy5 <- plot_quadrant_heatmap(cd_df, panel_tag = "(e)", heavy_cutoff = 5L, x_max = heavy_days_max)
-p2f_heavy5 <- plot_quadrant_heatmap(fp_df_cd_thresholds, panel_tag = "(f)", heavy_cutoff = 5L, x_max = heavy_days_max)
-
 col_left <- make_column_header("Current Day") / p2a / p2c / p2e + plot_layout(heights = c(0.09, 1, 1, 1))
 col_right <- make_column_header("Future Projection") / p2b / p2d / p2f + plot_layout(heights = c(0.09, 1, 1, 1))
 
 figure2_plot <- col_left | col_right
 figure2_plot
+
+# Figure 5: at least 5 Heavy days
+p2e_heavy5 <- plot_quadrant_heatmap(cd_df, panel_tag = "(e)", heavy_cutoff = 5L, x_max = heavy_days_max)
+p2f_heavy5 <- plot_quadrant_heatmap(fp_df_cd_thresholds, panel_tag = "(f)", heavy_cutoff = 5L, x_max = heavy_days_max)
 
 col_left_heavy5 <- make_column_header("Current Day") / p2e_heavy5 + plot_layout(heights = c(0.09, 1))
 col_right_heavy5 <- make_column_header("Future Projection") / p2f_heavy5 + plot_layout(heights = c(0.09, 1))
@@ -649,7 +648,7 @@ figure2_heavy5_plot
 # Save outputs -------------------------------------------------------------
 ggsave(filename = figure1_file, plot = figure1_plot, width = 7, height = 12, dpi = 2000)
 ggsave(filename = figure2_file, plot = figure2_plot, width = 11, height = 10.4, dpi = 2000)
-ggsave(filename = figure5_file, plot = figure2_heavy5_plot, width = 11, height = 5.4, dpi = 2000)
+ggsave(filename = figure5_file, plot = figure2_heavy5_plot, width = 11, height = 4.5, dpi = 2000)
 
 
 # Simple min/max helpers for Figure 1b text -------------------------------
@@ -672,4 +671,3 @@ chiltern_record_years <- chiltern_record_end_year - chiltern_record_start_year +
 
 chiltern_record_start_year 
 chiltern_record_end_year
-
